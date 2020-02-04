367e706dd70282c2cbdbdf3439d0a62e91cdcef5
Kent Johnson <kent3737@gmail.com>
2020-02-03 20:27:37 -0500

    Add track file for one day

--- mapdeck_play.R
+++ mapdeck_play.R
@@ -1,12 +1,14 @@
 library(mapdeck)
-set_token('pk.eyJ1Ijoia2VudDM3IiwiYSI6ImNrNjMzbGd1NjA1dHUzbG84NW95ZTkwbjIifQ.VyDMAChBzOTXjP0bEVsKEQ')
 
 library(sf)
-tracks = st_read('~/Dev/Fair Skies-TEKKK/TEKKK_2019_03.gpkg')
+tracks = st_read('~/Dev/Fair Skies-TEKKK-ADSB/TEKKK_2019_03.gpkg')
 one_day = tracks[as.Date(tracks$time)=='2019-03-11',]
-
+st_write(one_day, 'tracks_2019_03_11.gpkg')
+one_day = st_read('tracks_2019_03_11.gpkg')
 mapdeck(location=c(-71.128184, 42.3769824), zoom=14) %>% 
   add_path(one_day)
+mapdeck(location=c(-71.128184, 42.3769824), zoom=14) %>% 
+  add_path(tracks)
 
 mapdeck(location=c(-71.128184, 42.3769824), zoom=10) %>% 
   add_trips(one_day)
